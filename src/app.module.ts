a6d3a9a3aa1431f1cceb3dd46bd1ed278f01b0b2
Josue ortiz <ortizjd0036@gmail.com>
2024-09-18 17:34:58 -0400

    feature:[db conected]

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,20 +11,24 @@ import { DataSource } from 'typeorm';
     ConfigModule.forRoot({
       envFilePath: 'deploy/.env',
     }),
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: process.env.DB_HOST,
-    //   port: parseInt(process.env.DB_PORT),
-    //   username: process.env.DB_USERNAME,
-    //   password: process.env.DB_PASSWORD,
-    //   database: process.env.DB_DATABASE,
-    //   entities: ['dist/**/*.entity{.ts,.js}'],
-    //   synchronize: true,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
   
-    // })
+    })
 
 ],
   controllers: [AppController],
   providers: [AppService],
 })
+
+
 export class AppModule {}
+
+

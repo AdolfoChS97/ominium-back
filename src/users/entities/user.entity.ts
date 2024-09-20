73eca021f61463558f782207022ec284f23e3231
Josue ortiz <ortizjd0036@gmail.com>
2024-09-20 12:20:05 -0400

    feature:[add property of @Column user]

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsNumber, IsString, MinLength } from 'class-validator';
 import {
   Column,
   CreateDateColumn,
@@ -19,38 +19,38 @@ export class User {
   @ApiProperty({ example: 'John', description: 'Name of the user' })
   @IsString()
   @Transform(({ value }) => value.trim())
-  @Column()
+  @Column("varchar", { length: 25 , nullable: false})
   name: string;
 
   @ApiProperty({ example: 'Doe', description: 'Last name of the user' })
   @IsString()
   @Transform(({ value }) => value.trim())
-  @Column()
+  @Column("varchar", { length: 20, nullable: false })
   last_name: string;
 
-  @ApiProperty({ example: '123456789', description: 'Phone number of the user' })
+  @ApiProperty({ example: '5812345678', description: 'Phone number of the user' })
   @IsString()
   @Transform(({ value }) => value.trim())
-  @Column()
+  @Column("varchar", { length: 15, nullable: false })
   phone_number: string;
 
   @ApiProperty({ example: 'johndoe@gmail.com', description: 'User name' })
   @IsEmail()
-  @Column()
+  @Column("varchar", { unique: true, nullable: false , length: 50})
   email: string;
 
   @ApiProperty({ example: 'username', description: 'Nickname of user' })
   @IsString()
   @MinLength(6)
   @Transform(({ value }) => value.trim())
-  @Column()
+  @Column("varchar", { unique: true, nullable: false, length: 30 })
   user_name: string;
 
   @ApiProperty({ example: 'password', description: 'User password' })
   @IsString()
   @MinLength(8)
   @Transform(({ value }) => value.trim())
-  @Column()
+  @Column( )
   password: string;
 
   @CreateDateColumn()
